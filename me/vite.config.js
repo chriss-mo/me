ae1a655d686e414f342bff625349b8501ae365d3
chriss-mo <156863651+chriss-mo@users.noreply.github.com>
2025-03-30 23:40:44 -0700

    Update vite.config.js

--- me/vite.config.js
+++ me/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/me/',
+  base: './',
   plugins: [react()],
   assetsInclude: ['**/*.JPG','**/*.PNG']
 })
