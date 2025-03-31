67d41d14f18cac5984604028c7fd94a040302114
chriss-mo <156863651+chriss-mo@users.noreply.github.com>
2025-03-31 01:00:03 -0700

    Update main.jsx

--- me/src/main.jsx
+++ me/src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './App.jsx';
 import './index.css';
 
@@ -9,9 +9,8 @@ const root = createRoot(container);
 
 root.render(
   <StrictMode>
-    {/* <BrowserRouter basename='/me'> */}
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 );
