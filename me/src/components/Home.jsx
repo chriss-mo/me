6f794e6a979e3659132eb9d4c2f03e4a343b077b
chriss-mo <156863651+chriss-mo@users.noreply.github.com>
2025-04-14 00:38:18 -0700

    REVERT IF BAD: add clustrmaps

--- me/src/components/Home.jsx
+++ me/src/components/Home.jsx
@@ -1,26 +1,39 @@
 // Home.jsx
-import React from "react";
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 import "./Home.css";
 
 function Home() {
-  return (
-    <div className="home">
-      <div className="home-container">
-        <h1 className="home-title">Hi, I'm Chris Mo.</h1>
-        <h2 className="home-subtitle">
-          Welcome to my website!
-        </h2>
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src = "//clustrmaps.com/globe.js?d=RMr6mls09Sr5ToV2sQP4jm8DgcDvbq3a8IY4obZdRCM";
+    script.type = "text/javascript";
+    script.id = "clstr_globe";
+    document.getElementById("home-globe-container").appendChild(script);
+  }, []);
 
-        <Link to="/about" className="button-link">
-          About Me
-        </Link>
-        <Link to="/projects" className="button-link">
-          My Projects
-        </Link>
+  return (
+    <div className="home-wrapper">
+      {/* Home Section */}
+      <div className="home">
+        <div className="home-container">
+          <h1 className="home-title">Hi, I'm Chris Mo.</h1>
+          <h2 className="home-subtitle">Welcome to my website!</h2>
+
+          <Link to="/about" className="button-link">
+            About Me
+          </Link>
+          <Link to="/projects" className="button-link">
+            My Projects
+          </Link>
+        </div>
       </div>
-      <div className="home-globe">
-        <script type="text/javascript" id="clstr_globe" src="//clustrmaps.com/globe.js?d=RMr6mls09Sr5ToV2sQP4jm8DgcDvbq3a8IY4obZdRCM"></script>
+
+      {/* Globe Section */}
+      <div id="home-globe-container" className="home-globe">
+        <div className="globe-title">
+          <h1>Site visitors:</h1>
+        </div>
       </div>
     </div>
   );
